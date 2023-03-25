20cb4ea1713116a1e8ad70534e65ec87cde15e57
DangThaiHoa <dangthaihoalol@gmail.com>
2023-03-25 16:22:32 +0700

    Complete: WeatherInformation 50%: ControlFragment Update gradle

--- app/src/main/java/com/example/homecontrol/MainActivity.java
+++ app/src/main/java/com/example/homecontrol/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.homecontrol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.homecontrol.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new WeatherInformationFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.weather:
+                        replaceFragment(new WeatherInformationFragment());
+                        break;
+                    case R.id.control:
+                        replaceFragment(new ControlFragment());
+                        break;
+
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
