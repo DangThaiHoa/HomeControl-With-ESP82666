ba9e33ce3ceadfb090105ca77c20cd1b777148bb
DangThaiHoa <dangthaihoalol@gmail.com>
2023-03-27 15:35:55 +0700

    Complete: WeatherInformationFragment

--- app/src/main/java/com/example/homecontrol/WeatherInformationFragment.java
+++ app/src/main/java/com/example/homecontrol/WeatherInformationFragment.java
@@ -1,9 +1,13 @@
 package com.example.homecontrol;
 
 import android.app.Dialog;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -11,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,6 +23,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,11 +64,11 @@ public class WeatherInformationFragment extends Fragment {
         return fragment;
     }
 
-    TextView temp,hum,errorDHT;
-    DatabaseReference refTemp,refHum,refErrorDHT;
+    TextView temp,hum,errorDHT,weather,date, time, errorWea;
+    DatabaseReference refTemp,refHum,refErrorDHT, refWaterSensor, refDate, refTime, refErrorDS1302;
     FirebaseDatabase database;
-
-    Dialog dialog;
+    CardView tempCard, humCard, weaCard;
+    ImageView weatherImage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -81,19 +88,45 @@ public class WeatherInformationFragment extends Fragment {
         temp = view.findViewById(R.id.Temperature);
         hum = view.findViewById(R.id.Humanity);
         errorDHT = view.findViewById(R.id.textDHTError);
+        weather = view.findViewById(R.id.Weather);
+        date = view.findViewById(R.id.Date);
+        time = view.findViewById(R.id.Time);
+        errorWea = view.findViewById(R.id.textWeatherError);
+
+        tempCard = view.findViewById(R.id.cardTemp);
+        humCard = view.findViewById(R.id.cardHum);
+        weaCard = view.findViewById(R.id.cardWeather);
+
+        weatherImage = view.findViewById(R.id.WeatherIcon);
 
         database = FirebaseDatabase.getInstance();
         refTemp = database.getReference("DHT11/temp");
         refHum = database.getReference("DHT11/hum");
         refErrorDHT = database.getReference("DHT11/error");
 
+        refWaterSensor = database.getReference("WATERSENSOR/waterdata");
+
+        refDate = database.getReference("DS1302/date");
+        refTime = database.getReference("DS1302/time");
+        refErrorDS1302 = database.getReference("DS1302/error");
+
+        ReadDHT11();
+
+        ReadDateTime();
+
+        ReadWeather("6");
+
+        return view;
+    }
+
+    private void ReadDHT11(){
         refErrorDHT.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 String getError = dataSnapshot.getValue(String.class);
-                if(getError == "Read/Connect"){
+                if(getError.equals("Read/Connect")){
                     errorDHT.setVisibility(View.VISIBLE);
                     temp.setText("Null");
                     hum.setText("Null");
@@ -106,6 +139,13 @@ public class WeatherInformationFragment extends Fragment {
                             // whenever data at this location is updated.
                             Float getTemp = dataSnapshot.getValue(Float.class);
                             temp.setText(getTemp.toString() + "\u2103");
+                            if(getTemp < 20){
+                                tempCard.setCardBackgroundColor(Color.parseColor("blue"));
+                            }else if(getTemp >= 20 && getTemp <= 35){
+                                tempCard.setCardBackgroundColor(Color.parseColor("green"));
+                            }else if(getTemp > 35){
+                                tempCard.setCardBackgroundColor(Color.parseColor("red"));
+                            }
                         }
 
                         @Override
@@ -120,6 +160,13 @@ public class WeatherInformationFragment extends Fragment {
                             // whenever data at this location is updated.
                             Float getHum = dataSnapshot.getValue(Float.class);
                             hum.setText(getHum.toString() + "%");
+                            if(getHum < 40){
+                                humCard.setCardBackgroundColor(Color.parseColor("blue"));
+                            }else if(getHum >= 40 && getHum <= 70){
+                                humCard.setCardBackgroundColor(Color.parseColor("green"));
+                            }else if(getHum > 70){
+                                humCard.setCardBackgroundColor(Color.parseColor("red"));
+                            }
                         }
                         @Override
                         public void onCancelled(@NonNull DatabaseError error) {
@@ -132,7 +179,98 @@ public class WeatherInformationFragment extends Fragment {
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
+    }
 
-        return view;
+    private void ReadDateTime(){
+
+        refErrorDS1302.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String getError = dataSnapshot.getValue(String.class);
+                if(getError.equals("Read/Connect/Battery")){
+                    errorWea.setVisibility(View.VISIBLE);
+                    date.setText("Null");
+                    time.setText("Null");
+                }else{
+                    errorWea.setVisibility(View.INVISIBLE);
+                    refDate.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            String getDate = dataSnapshot.getValue(String.class);
+                            date.setText(getDate);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                        }
+                    });
+
+                    refTime.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            String getTime = dataSnapshot.getValue(String.class);
+                            time.setText(getTime);
+                            ReadWeather(getTime);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void ReadWeather(String time){
+        refWaterSensor.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int getWaterData = dataSnapshot.getValue(Integer.class);
+                String string = time;
+                String[] parts = string.split(":");
+                String hour = parts[0];
+                if(Integer.parseInt(hour) >= 6 && Integer.parseInt(hour) < 18){
+                    if(getWaterData <= 50){
+                        weather.setText("Nắng");
+                        weatherImage.setImageResource(R.drawable.sun);
+                        weaCard.setCardBackgroundColor(Color.parseColor("yellow"));
+                    }else if (getWaterData >= 50 && getWaterData <= 350){
+                        weather.setText("Mưa Nhỏ");
+                        weatherImage.setImageResource(R.drawable.raining);
+                        weaCard.setCardBackgroundColor(Color.parseColor("blue"));
+                    }else{
+                        weather.setText("Mưa Rào");
+                        weatherImage.setImageResource(R.drawable.storm);
+                        weaCard.setCardBackgroundColor(Color.parseColor("black"));
+                    }
+                }else{
+                    if(getWaterData <= 50){
+                        weather.setText("Đêm");
+                        weatherImage.setImageResource(R.drawable.moon);
+                        weaCard.setCardBackgroundColor(Color.parseColor("black"));
+                    }else if (getWaterData >= 50 && getWaterData <= 350){
+                        weather.setText("Mưa Nhỏ");
+                        weatherImage.setImageResource(R.drawable.raining);
+                        weaCard.setCardBackgroundColor(Color.parseColor("blue"));
+                    }else{
+                        weather.setText("Mưa Rào");
+                        weatherImage.setImageResource(R.drawable.storm);
+                        weaCard.setCardBackgroundColor(Color.parseColor("black"));
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
